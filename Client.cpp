5bf9625533ccc679b86b180a00cad77054dd0247
Kirsten <kboyles138@gmail.com>
2022-03-04 22:09:46 -0800

    good enoughu

--- Client.cpp
+++ Client.cpp
@@ -15,72 +15,81 @@ using namespace std;
 //#define PORT_NUMBER 5437
 char *IP_AD;
 int PORT;
+int burger;
 
-
-bool isValidNum(string str) {
-    for (int i = 0; i < str.length(); i++) {
-        if (! isdigit(str[i])) {
+bool isValidNum(string str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (!isdigit(str[i]))
+        {
             return false;
         }
     }
 
-    try {
+    try
+    {
         if (stoi(str) <= 0)
             return false;
     }
-    catch (invalid_argument& e) {
+    catch (invalid_argument &e)
+    {
         return false;
     }
 
     return true;
 }
 
-
-bool isValid(char** args){
+bool isValid(char **args)
+{
     struct sockaddr_in sa;
     int result = inet_pton(AF_INET, args[1], &(sa.sin_addr));
 
-    if (result == 0) {
+    if (result == 0)
+    {
         printf("\nIP address entered is invalid\n\n");
         return false;
     }
-    else if(result < 0){
+    else if (result < 0)
+    {
         printf("\n\nIP address entered is not part of a valid address family\n\n");
         return false;
     }
-    else if(!isValidNum(args[2]) || stoi(string(args[2])) <= 0){
+    else if (!isValidNum(args[2]) || stoi(string(args[2])) <= 0)
+    {
         printf("\n\nPort is not a valid number\n\n");
         return false;
     }
-    else if(!isValidNum(args[3]) || stoi(string(args[3])) <= 0){
+    else if (!isValidNum(args[3]) || stoi(string(args[3])) <= 0)
+    {
         printf("\n\nNumber not given for amount of burgers client can eat\n\n");
         return false;
     }
     return true;
 }
 
-
-
 int main(int argc, char *argv[])
 {
-     if (argc != 4)
+    if (argc != 4)
     {
         printf("\n Usage: %s <ip of server> <port number> <amount of burgers the client can eat> \n", argv[0]);
         return 1;
     }
-    if (!isValid(argv)){
+    if (!isValid(argv))
+    {
         return 1;
     }
+    // connecting to server
     int sockfd = 0, n = 0;
 
     char recvBuff[BUFFER_SIZE];
     struct sockaddr_in serv_addr;
     pid_t clientPID = getpid();
     PORT = stoi(string(argv[2]));
+    burger = stoi(argv[3]);
 
     IP_AD = argv[1];
-   
-   
+
     memset(recvBuff, '0', sizeof(recvBuff));
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
@@ -99,6 +108,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // 4 connection attempts with 3 seconds between each interval. exits after the 4th attempt
     for (int i = 0; i < 4; i++)
     {
         if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
@@ -115,33 +125,67 @@ int main(int argc, char *argv[])
         else
         {
             printf("\n[Client #%d] Connection to server established at %s:%d\n", clientPID, IP_AD, PORT);
-            printf("est");
             break;
         }
     }
 
-    if (write(sockfd, &clientPID, sizeof(clientPID)) < 0)
-    {
-        fprintf(stderr, "[Client #%d] Error: failed to send pid to server\n%s\n", clientPID, strerror(errno));
-    }
-    int ACK = 1;
-    if(write(sockfd, &ACK, sizeof(ACK)) < 0){
-        fprintf(stderr, "[Client #%d] Error: failed to send ACK for welcome message to server\n%s\n", clientPID, strerror(errno));
-    }
+    // sending the client pid to the server
+    send(sockfd, &clientPID, sizeof(clientPID), 0);
 
+    //
 
-    while ((n = read(sockfd, recvBuff, sizeof(recvBuff) - 1)) > 0)
+    while (burger != 0)
     {
+
+        // char statusBuff[BUFFER_SIZE];
+        //  recieve burger total from shop
+        memset(recvBuff, '0', sizeof(recvBuff));
+
+        n = read(sockfd, recvBuff, sizeof(recvBuff));
         recvBuff[n] = 0;
-        if (fputs(recvBuff, stdout) == EOF)
+        if (!isValidNum(recvBuff))
         {
-            printf("\n Error : Fputs error\n");
+            printf("\nNo more burgers left :(\n");
+            break;
         }
-    }
+        
 
-    if (n < 0)
-    {
-        printf("\n Read error \n");
+        printf("\n\n[Client #%d] There are %s burgers left.\n", clientPID, recvBuff);
+        // send request for burger
+
+        memset(recvBuff, '0', sizeof(recvBuff));
+
+        sprintf(recvBuff, "%d", burger);
+
+        send(sockfd, recvBuff, sizeof(recvBuff), 0);
+        printf("\n[Client #%d] Request for burger sent.\n", clientPID);
+
+        memset(recvBuff, '0', sizeof(recvBuff));
+        n = read(sockfd, recvBuff, sizeof(recvBuff));
+        if (n = 0)
+        {
+            printf("\nSERVER - Connection to the client was lost\n");
+            return 1;
+        }
+        if (n < 0)
+        {
+            fprintf(stderr, "SERVER - Error: failed to retrieve PID from the client\n%d\n", errno);
+            return 1;
+        }
+        recvBuff[n] = 0;
+        printf("%s\n", recvBuff);
+
+        srand(time(NULL));
+        int wait = rand() % 5 + 4;
+        sleep(wait);
+
+        memset(recvBuff, '0', sizeof(recvBuff));
+
+        int ACK = 1;
+        send(sockfd, &ACK, sizeof(ACK), 0);
+
+        burger--;
+        printf("\n[Client #%d] I ate my burger, I want %d more burger(s).\n", clientPID, burger);
     }
 
     close(sockfd);
